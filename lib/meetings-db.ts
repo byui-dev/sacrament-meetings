1696754529d28892f9da05cb035717d75df58c09
byui-dev <okdiphala@hotmail.com>
2026-09-18 22:16:57 +0200

    Refactor lib/meetings-db.ts to ensure thatv value of id is used after it has been declared

--- lib/meetings-db.ts
+++ lib/meetings-db.ts
@@ -29,5 +29,5 @@ export function getMeetings(date?: string | null): SacramentMeeting[] {
 };
 
 export function getMeetingById(id: number): | SacramentMeeting | null {
-    return meetings.find(m => m.id) ?? null;
+    return meetings.find(m => m.id === id) ?? null;
 }    
